2ee6aec31b3d56fea453c6ded478ffb24a16322e
Merle <57601115+mcarr10@users.noreply.github.com>
2022-04-04 22:05:30 -0400

    Update Example-ApproximateRowCountsLargeTables.sql

--- Example-ApproximateRowCountsLargeTables.sql
+++ Example-ApproximateRowCountsLargeTables.sql
@@ -2,7 +2,7 @@
 
 SELECT SCHEMA_NAME(so.schema_id) As SchemaName
 ,so.[name] AS TableName
-,ps.row_count
+,ps.RowCount
 FROM sys.indexes AS si
 INNER JOIN sys.objects AS so ON si.object_id = so.object_id
 INNER JOIN sys.dm_db_partition_stats AS ps ON si.object_id = ps.object_id AND si.index_id = ps.index_id
