4b8bfa8aa562280d3a3ec2cfee08fd6acc9d5c45
Merle <57601115+mcarr10@users.noreply.github.com>
2022-04-04 22:01:55 -0400

    Create Example-ApproximateRowCountsLargeTables.sql
    Approximate table row count query for large tables and databases.

---
+++ Example-ApproximateRowCountsLargeTables.sql
@@ -0,0 +1,10 @@
+/* This is an efficient query for approximate row counts on large tables.  The results can vary from the actual numer of rows in a given table. */
+
+SELECT SCHEMA_NAME(so.schema_id) As SchemaName
+,so.[name] AS TableName
+,ps.row_count
+FROM sys.indexes AS si
+INNER JOIN sys.objects AS so ON si.object_id = so.object_id
+INNER JOIN sys.dm_db_partition_stats AS ps ON si.object_id = ps.object_id AND si.index_id = ps.index_id
+WHERE si.index_id < 2
+AND so.is_ms_shipped = 0
