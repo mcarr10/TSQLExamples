40bc17175179bd905b35528e87e02ffbf94a453d
Merle <57601115+mcarr10@users.noreply.github.com>
2022-04-04 22:05:57 -0400

    Update Example-ApproximateRowCountsLargeTables.sql

--- Example-ApproximateRowCountsLargeTables.sql
+++ Example-ApproximateRowCountsLargeTables.sql
@@ -1,8 +1,8 @@
 /* This is an efficient query for approximate row counts on large tables.  The results can vary from the actual numer of rows in a given table. */
 
-SELECT SCHEMA_NAME(so.schema_id) As SchemaName
+SELECT SCHEMA_NAME(so.schema_id) AS SchemaName
 ,so.[name] AS TableName
-,ps.RowCount
+,ps.row_count AS RowCount
 FROM sys.indexes AS si
 INNER JOIN sys.objects AS so ON si.object_id = so.object_id
 INNER JOIN sys.dm_db_partition_stats AS ps ON si.object_id = ps.object_id AND si.index_id = ps.index_id
