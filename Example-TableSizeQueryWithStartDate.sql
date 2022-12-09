c62c69fc0852d84604342106ec280c83de0d2bdf
Merle <57601115+mcarr10@users.noreply.github.com>
2022-12-09 11:14:26 -0500

    Create Example-TableSizeQueryWithStartDate.sql
    Table size query with create date where clause.

---
+++ Example-TableSizeQueryWithStartDate.sql
@@ -0,0 +1,19 @@
+/* This is an example query to return table sizes.  The where clause is to return only recently created tables.*/
+
+DECLARE @StartDate AS DATE = GETDATE()-2000
+SELECT
+t.create_date As CreateDate
+,s.name As SchemaName
+,t.name AS TableName
+,p.rows AS RowCounts
+,CAST(ROUND((SUM(a.used_pages)/128.00),2) AS NUMERIC(36,2)) As UsedMB
+,CAST(ROUND((SUM(a.total_pages)/128.00),2) AS NUMERIC(36,2)) As TotalMB
+FROM sys.tables t
+INNER JOIN sys.indexes i on t.object_id = i.object_id
+INNER JOIN sys.partitions p on i.object_id = p.object_id AND i.index_id = p.index_id
+INNER JOIN sys.allocation_units a on p.partition_id = a.container_id
+INNER JOIN sys.schemas s ON t.schema_id = s.schema_id
+WHERE t.create_date >= @StartDate
+GROUP BY t.create_date, t.Name, s.Name, p.rows
+ORDER BY TotalMB DESC
+GO
