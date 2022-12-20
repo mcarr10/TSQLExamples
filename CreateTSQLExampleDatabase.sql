dfeaee84f91565ef7dd50c98ef6690df41a36ccf
Merle <57601115+mcarr10@users.noreply.github.com>
2022-12-20 12:14:55 -0500

    Added table and ISO query
    Added date example table to TSQLExample
    ISO8601 query example

--- CreateTSQLExampleDatabase.sql
+++ CreateTSQLExampleDatabase.sql
@@ -1,4 +1,6 @@
 /* Create TSQLExample database used by some other examples */
+/* 2022-12-20 Added DateExample table */
+
 USE master
 GO
 IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = 'TSQLExample')
@@ -47,3 +49,24 @@ BEGIN
 	INSERT [dbo].[TableC] ([Category], [Date], [TableCCount]) VALUES (N'BlueWidgets', CAST(N'2022-01-12' AS Date), 20)	
 	INSERT [dbo].[TableC] ([Category], [Date], [TableCCount]) VALUES (N'GreenWidgetS', CAST(N'2022-01-12' AS Date), 20)
 END
+
+IF NOT EXISTS (SELECT 1 FROM [dbo].[DateExample])
+BEGIN
+CREATE TABLE [dbo].[DateExample](
+	[DateExampleID] [int] IDENTITY(1,1) NOT NULL,
+	[DateExample] [date] NULL,
+	[DatetimeExample] [datetime] NULL,
+	[Datetime2Example] [datetime2](7) NULL,
+ CONSTRAINT [PK_DateExample] PRIMARY KEY CLUSTERED 
+(
+	[DateExampleID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+SET IDENTITY_INSERT [dbo].[DateExample] ON 
+GO
+INSERT [dbo].[DateExample] ([DateExampleID], [DateExample], [DatetimeExample], [Datetime2Example]) VALUES (1, CAST(N'2022-12-01' AS Date), CAST(N'2022-12-20T12:05:06.130' AS DateTime), CAST(N'2022-12-20T12:05:51.4733333' AS DateTime2))
+GO
+SET IDENTITY_INSERT [dbo].[DateExample] OFF
+GO
---
+++ Example-ConvertDateTimeToISO8601Format.sql
@@ -0,0 +1,3 @@
+USE TSQLExample
+SELECT CONVERT(VARCHAR(33), DatetimeExample, 126) AS ISO8601Format
+FROM [dbo].[DateExample]
